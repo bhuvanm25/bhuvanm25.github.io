80ef418a9fdf11f1c2bd61da2842009d6396f742
Bhuvan Mohan <129832468+bhuvanm25@users.noreply.github.com>
2026-02-11 22:15:48 -0500

    Add files via upload

---
+++ assets/js/projects-slider.js
@@ -0,0 +1,30 @@
+/*--------------- projects Slider ---------------*/
+var swiper = new Swiper(".projects-slider", { 
+
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        display: 2500,
+        disableOnInteraction: false,
+    },
+    
+    pagination: {
+        el: ".swiper-pagination2",
+        clickable:true,
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+
+        768: {
+            slidesPerView: 2,
+        },
+
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+
+});
